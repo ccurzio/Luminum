e28d2953c23130fcea6bac956899aae573b7c923
Christopher R. Curzio <ccurzio@accipiter.org>
2024-06-07 19:10:51 -0400

    Added localhost listener socket for IPC to linux client

--- client/linux/src/main.rs
+++ client/linux/src/main.rs
@@ -6,12 +6,15 @@ use colored::Colorize;
 use chrono::Local;
 use chrono::format::strftime::StrftimeItems;
 use std::process;
+use std::net::TcpListener;
 use rusqlite::{params, Connection, Result};
 use std::collections::HashMap;
-use std::fs::{self, File};
+use std::fs;
+use std::io::{Read};
 
 const VER: &str = "0.0.1";
 const CFGPATH: &str = "/opt/luminum/LuminumClient/conf/luminum.conf.db";
+const LPORT: u16 = 10511;
 
 fn main() {
 	let matches = App::new("Luminum Client (Linux)")
@@ -32,6 +35,8 @@ fn main() {
 	// Client Startup
 	dbout(debug,0,format!("Starting Luminum Client v{}...",VER).as_str());
 
+	lumcomm(debug);
+
 	if fs::metadata(CFGPATH).is_ok() {
 		// Read Client Config
 		let confconn = Connection::open(CFGPATH).expect("Error: Could not open configuration database.");
@@ -39,6 +44,22 @@ fn main() {
 	else {
 		dbout(debug,1,format!("Configuration database not found.").as_str());
 		process::exit(1);
+		}
+	}
+
+fn lumcomm(debug: bool) {
+	let listener = TcpListener::bind("127.0.0.1:10511").expect("Error: Could not open listener on localhost");
+	for stream in listener.incoming() {
+		match stream {
+			Ok(mut stream) => {
+				let mut buffer = [0; 1024];
+				let bytes_read = stream.read(&mut buffer).expect("Failed to read from socket");
+				println!("Received: {}", String::from_utf8_lossy(&buffer[..bytes_read]));
+				}
+			Err(e) => {
+				eprintln!("Error: {}", e);
+				}
+			}
 		}
 	}
 
