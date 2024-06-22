851b61ee041777e955f0b4693a0e692cf6199228
Christopher R. Curzio <ccurzio@accipiter.org>
2024-06-22 07:15:22 -0400

    Implemented Integrity config fetching in linux client; Imporoved variable handling in linux client

--- client/modules/linux/Integrity/src/main.rs
+++ client/modules/linux/Integrity/src/main.rs
@@ -1,4 +1,8 @@
 use std::fs::{self, File};
+use std::os::unix::fs::MetadataExt;
+use std::os::unix::fs::PermissionsExt;
+use users::get_group_by_gid;
+use users::get_user_by_uid;
 use std::env;
 use std::process;
 use std::path::Path;
@@ -11,7 +15,9 @@ use notify::{RecommendedWatcher, RecursiveMode, Watcher, Event, Config, Result a
 use notify::event::{EventKind, MetadataKind};
 use std::sync::mpsc::channel;
 use std::time::Duration;
+use std::time::{SystemTime, UNIX_EPOCH};
 use serde::{Serialize, Deserialize};
+use serde_json::{json, Value};
 use rmp_serde::{from_read, Deserializer, Serializer, to_vec_named};
 use rmp_serde::decode::from_slice;
 
@@ -21,114 +27,115 @@ struct NotifyEvent {
 	paths: Vec<String>
 	}
 
-#[derive(Serialize, Deserialize, Debug)]
-struct LumyMessage {
-	lumy: String,
-	version: String,
-	content: LumyContent
-	}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct LumyContent {
-	action: String
-	}
-
 impl From<Event> for NotifyEvent {
 	fn from(event: Event) -> Self {
 		NotifyEvent {
 			kind: format!("{:?}", event.kind),
-			paths: event.paths.into_iter().map(|p| p.to_string_lossy().into_owned()).collect(),
+			paths: event.paths.into_iter().map(|p| p.to_string_lossy().into_owned()).collect()
 			}
 		}
 	}
 
 const VER: &str = "0.0.1";
 const CFGPATH: &str = "/opt/Luminum/LuminumClient/modules/integrity/integrity.conf.db";
+const IMLOGS: &str = "/opt/Luminum/LuminumClient/modules/integrity/imlogs.db";
 
 fn main() {
-	let mut lumyconfig: HashMap<String, String> = HashMap::new();
+	let mut has_config: bool;
 
 	if file_exists(CFGPATH) {
+		has_config = true;
 		}
 	else {
+		has_config = false;
 		}
 
 	if is_inotify_enabled() {
-		let (tx, rx) = channel();
 
+		let (tx, rx) = channel();
 		let mut watcher: RecommendedWatcher = RecommendedWatcher::new(tx, Config::default()).expect("Error: Could not set up watcher.");
-		let watchpaths = vec![ "/usr", "/usr/bin" ];
+
+		let watchpaths = get_config("watch");
+		let ignorepaths = get_config("ignore");
 
 		for watchpath in watchpaths {
-			watcher.watch(Path::new(watchpath), RecursiveMode::Recursive);
+			println!("{:?}",watchpath);
+			watcher.watch(Path::new(&watchpath), RecursiveMode::Recursive);
 			}
 
-		let mut stream = match TcpStream::connect("127.0.0.1:10461") {
-			Ok(_) => {
-				//println!("Connected to Luminum Client");
-				}
-			Err(err) => { 
-				println!("Error: Could not connect to Luminum Client process: {}", err);
-				}
-			};
+		for ignorepath in ignorepaths {
+			
 
-/*
-		if !file_exists(CFGPATH) {
-			let mut combined_json = json!({});
-			combined_json["product"] = serde_json::to_value("Luminum Integrity").unwrap();
-			combined_json["version"] = serde_json::to_value(VER).unwrap();
-			combined_json["content"]["action"] = serde_json::to_value("config").unwrap();
-			combined_json["content"]["info"] = serde_json::to_value("none").unwrap();
-			let json_event = serde_json::to_string(&combined_json).unwrap();
-			stream.write_all(&json_event.as_bytes());
-			println!("{}",json_event);
-			println!("Fatal: The Integrity Lumy configuration is missing.");
-			process::exit(1);
-			}
-		else {
-			let mut combined_json = json!({});
-			combined_json["product"] = serde_json::to_value("Luminum Integrity").unwrap();
-			combined_json["version"] = serde_json::to_value(VER).unwrap();
-			combined_json["content"]["status"] = serde_json::to_value("running").unwrap();
-			let json_event = serde_json::to_string(&combined_json).unwrap();
-			stream.write_all(&json_event.as_bytes());
-			}
-*/
 		loop {
 			match rx.recv() {
 				Ok(Ok(event)) => {
-					/*
+					println!("{:?}", event);
+					let notify_event: NotifyEvent = event.clone().into();
 					let mut combined_json = json!({});
-					let notify_event: NotifyEvent = event.into();
-					let eijson = json!({"dtype":"inotify"});
-					let event_info: Value = serde_json::to_value(eijson).unwrap();
-					combined_json["product"] = serde_json::to_value("Luminum Integrity").unwrap();
-					combined_json["content"]["notify_event"] = serde_json::to_value(&notify_event).unwrap();
-					combined_json["content"]["event_info"] = serde_json::to_value(&event_info).unwrap();
+					combined_json["notify_event"] = serde_json::to_value(&notify_event).unwrap();
 					let json_event = serde_json::to_string(&combined_json).unwrap();
-					//let json_event_with_newline = format!("{}\n", json_event);
-					if let Err(e) = stream.write_all(json_event.as_bytes()) {
-						eprintln!("Failed to send message: {}", e);
-						break;
+					if let Ok(metadata) = std::fs::metadata(&event.paths[0].clone()) {
+						println!("{:?}",event.kind);
+						println!("{:?}",metadata);
+						println!("{:?}",event.paths);
+                                        	let mut ftype = String::new();
+						if metadata.is_dir() { ftype = "Directory".to_string() }
+						else if metadata.is_file() {ftype = "File".to_string() }
+						println!("{}",String::from(ftype));
+						let permissions = metadata.permissions();
+						let mode = permissions.mode();
+						println!("{}",octal_to_symbolic(mode));
+						let user = get_user_by_uid(metadata.uid()).map_or("Unknown".to_string(), |u| u.name().to_string_lossy().into_owned());
+						let group = get_group_by_gid(metadata.gid()).map_or("Unknown".to_string(), |g| g.name().to_string_lossy().into_owned());
+						println!("{}:{}",user,group);
+						let file_size = metadata.len();
+						println!("{} bytes",file_size);
 						}
-
-					if let Err(e) = stream.flush() {
-						eprintln!("Failed to flush stream: {}", e);
-						break;
+					else {
+						println!("{:?}",event.kind);
+						println!("{:?}",event.paths);
 						}
-					println!("{}", json_event);
-					*/
 					}
 				Ok(Err(e)) => println!("Watch error: {:?}", e),
 				Err(e) => {
 					println!("Error: {:?}",e);
 					}
 				}
-			continue;
-			//stream.shutdown(Shutdown::Write);
 			}
 		}
 	}
+
+fn get_config(list: &str) -> Vec<String> {
+	if list.to_string() == "watch" {
+		let confconn = Connection::open(CFGPATH).expect("Error: Could not open configuration database.");
+		let mut stmt = confconn.prepare("select PATH from WATCH").expect("Failed to prepare query");
+		let watchpaths: Vec<String> = stmt.query_map([], |row| row.get(0)).expect("Failed to execute query").map(|result| result.expect("Error retrieving column value")).collect();
+		//confconn.close().unwrap();
+		watchpaths
+		}
+	else if list.to_string() == "ignore" {
+		let ignorepaths: Vec<String> = Vec::new();
+
+		ignorepaths
+		}
+	else {
+		Vec::new()
+		}
+	}
+
+fn save_event() {
+	let imlogsconn = Connection::open(IMLOGS).expect("Error: Could not open events database.");
+	imlogsconn.execute("create table if not exists EVENTS (`DATE` datetime not null, `TYPE` tinytext not null, `PATH` text not null, `PERMS` tinytext not null, `CHANGE` tinytext not null)",[]).expect("Error: Could not create EVENTS table in Integrity Lumy database");
+	imlogsconn.execute("insert into EVENTS (DATE) values (CURRENT_TIMESTAMP)",[]).expect("Error: Could not save event to Integrity Lumy database");
+	imlogsconn.close().unwrap();
+	}
+/*
+					let mut stream = TcpStream::connect("127.0.0.1:10461").expect("Error: Could not connect to Luminum Client process");
+					let notify_event: NotifyEvent = event.into();
+
+					stream.write_all(&json_event.as_bytes());
+					stream.shutdown(Shutdown::Write);
+*/
 
 fn is_inotify_enabled() -> bool {
 	fs::metadata("/proc/sys/fs/inotify").is_ok()
@@ -137,3 +144,22 @@ fn is_inotify_enabled() -> bool {
 fn file_exists(path: &str) -> bool {
         fs::metadata(path).is_ok()
         }
+
+fn octal_to_symbolic(octal: u32) -> String {
+	let mut result = String::new();
+
+    result.push(if (octal & 0o400) != 0 { 'r' } else { '-' });
+    result.push(if (octal & 0o200) != 0 { 'w' } else { '-' });
+    result.push(if (octal & 0o100) != 0 { 'x' } else { '-' });
+
+    // Group permissions
+    result.push(if (octal & 0o040) != 0 { 'r' } else { '-' });
+    result.push(if (octal & 0o020) != 0 { 'w' } else { '-' });
+    result.push(if (octal & 0o010) != 0 { 'x' } else { '-' });
+
+    // Other permissions
+    result.push(if (octal & 0o004) != 0 { 'r' } else { '-' });
+    result.push(if (octal & 0o002) != 0 { 'w' } else { '-' });
+    result.push(if (octal & 0o001) != 0 { 'x' } else { '-' });
+	result
+	}
