015b6e4a2d5953fba356715dc72dde24263bd22d
Christopher R. Curzio <ccurzio@accipiter.org>
2025-05-24 06:37:15 -0400

    Removed reissue button; added Status column

--- server/console/html/admin/actions.php
+++ server/console/html/admin/actions.php
@@ -10,20 +10,19 @@ $actioncount = 0;
 	<h1>Scheduled Actions</h1>
 	<div class="module-content">
 		<div style="display: block; width: 100%; text-align: right;">
-			<button class="formgo" style="margin-top: 5px; margin-right: 0;" disabled="disabled">Reissue</button>
 			<button class="formgo" style="margin-top: 5px; margin-right: 0;" disabled="disabled">Modify Selected</button>
 			<button class="formgo" style="margin-top: 5px; margin-right: 0;" disabled="disabled">Delete Selected</button>
 			<button class="formgo" style="margin-top: 5px; margin-right: 0;" disabled="disabled">Get Info</button>
 		</div>
 		<table id="satable" style="margin-top: 10px;">
-		<tr><td colspan="9"><div style="position: absolute; padding-top: 5px; padding-left: 5px;">0 of 0 items <img id="refresh" src="icons/refresh.png" style="cursor: pointer; margin-left: 2px; width: 20px; height: 20px; vertical-align: text-bottom;" onclick="reloadTable()"></div><div style="float: right; text-align: right; padding-right: 5px;">Filter: <input type="text" style="font-size: 15px; padding: 3px; margin-top: 0;" <?php if ($actioncount == 0) { print "disabled=\"disabled\""; } ?>></div></td></tr>
+		<tr><td colspan="10"><div style="position: absolute; padding-top: 5px; padding-left: 5px;">0 of 0 items <img id="refresh" src="icons/refresh.png" style="cursor: pointer; margin-left: 2px; width: 20px; height: 20px; vertical-align: text-bottom;" onclick="reloadTable()"></div><div style="float: right; text-align: right; padding-right: 5px;">Filter: <input type="text" style="font-size: 15px; padding: 3px; margin-top: 0;" <?php if ($actioncount == 0) { print "disabled=\"disabled\""; } ?>></div></td></tr>
 		<tr><td style="width: 15px;">
 		<?php
 		if ($actioncount == 0) { print "<input type=\"checkbox\" disabled=\"disabled\">"; }
 		else { print "<input type=\"checkbox\">"; }
 		?>
-		</td><td style="width: 30px; text-align: center;">Enabled</td><td style="width: 300px;">Name</td><td style="width: 75px;">Type</td><td style="width: 100px;">Issuer</td><td style="width: 175px;">Start Date</td><td style="width: 175px;">End Date</td><td style="width: 175px;">Last Modification</td><td style="width: 100px;">Modified By</tr>
-		<tr><td colspan="9" style="text-align: center; background-color: #494a69; font-weight: normal; font-style: italic;">No Results</td></tr>
+		</td><td style="width: 30px; text-align: center;">Enabled</td><td style="width: 30px;">Status</td><td style="width: 300px;">Name</td><td style="width: 75px;">Type</td><td style="width: 100px;">Issuer</td><td style="width: 175px;">Start Date</td><td style="width: 175px;">End Date</td><td style="width: 175px;">Last Modification</td><td style="width: 100px;">Modified By</tr>
+		<tr><td colspan="10" style="text-align: center; background-color: #494a69; font-weight: normal; font-style: italic;">No Results</td></tr>
 		<tr style="height: 35px;"><td colspan="10"><div style="position: absolute; padding-top: 5px;"></div><div style="float: right; text-align: right; padding-bottom: 2px; padding-right: 5px; font-weight: normal;"><i>Query Completed in <?php print $duration; ?> Seconds</i></div></td></tr>
 		</table>
 	</div>
