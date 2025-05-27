8167877f2ecbb00e1f58f35c89369fda284ae140
Christopher R. Curzio <ccurzio@accipiter.org>
2025-05-26 23:54:35 -0400

    Revised Account Profile page, creating a form with input fields for account values

--- server/console/html/user.php
+++ server/console/html/user.php
@@ -3,7 +3,7 @@
 <?php if (!isset($_GET['view']) || $_GET['view'] == "profile"): ?>
 <?php
         mysqli_select_db($db, "AUTH") or die( "<h5>Fatal Error</h5>\n\n<p>Unable to access database.\n</p>");
-        $userquery = mysqli_query($db, "select ID,USERNAME,FULLNAME,EMAIL,TYPE,ROLE,REGDATE from USERS where USERNAME = (select USERNAME from USERS where ID = (select ID from SESSION where SID = '" . $_SESSION["SID"] . "'))");
+        $userquery = mysqli_query($db, "select ID,USERNAME,FULLNAME,EMAIL,PHONE,TYPE,ROLE,REGDATE from USERS where USERNAME = (select USERNAME from USERS where ID = (select ID from SESSION where SID = '" . $_SESSION["SID"] . "'))");
         $userinfo = $userquery->fetch_assoc();
 	if ($userinfo["ROLE"] == "1") { $userrole = "Administrator"; }
 	elseif ($userinfo["ROLE"] == "2") { $userrole = "Power User"; }
@@ -14,13 +14,23 @@
 <div class="content">
 	<h1>Account Profile</h1>
 
-	<div class="module-content" style="width: 60%; margin-left: auto; margin-right: auto;">
-		<div style="margin-top: 20px;">
-			<h3><?php print "UID #" . $userinfo["ID"] . ": " . $userinfo["USERNAME"] . " (" . $userrole . ")"; ?></h3>
-			<span style="font-weight: bold;">Name: </span> <?php print $userinfo["FULLNAME"]; ?> <br>
-			<span style="font-weight: bold;">Email: </span> <?php print $userinfo["EMAIL"]; ?> <br>
-			<span style="font-weight: bold;">Registered: </span> <?php print $userinfo["REGDATE"]; ?> <br>
-			<span style="font-weight: bold;">Account Type: </span> <?php print $userinfo["TYPE"]; ?> <br>
+	<div class="module-content" style="width: 75%; margin-left: auto; margin-right: auto;">
+		<div style="display: block; width: 100%; text-align: right;">
+			<button id="saveuser" class="formgo" style="margin-top: 5px; margin-right: 0;" disabled="disabled">Save Changes</button>
+		</div>
+		<div style="margin-top: 10px;">
+			<table style="width: 75%; border: 0; margin-left: auto; margin-right: auto; margin-top: 15px; margin-bottom: 45px;">
+                        <tr><td style="color: #444; background-color: transparent; border: 0; font-weight: bold;">Userame:</td><td style="color: #444; background-color: transparent; border: 0; font-weight: normal;"> <input id="fullname" type="text" value="<?php print $userinfo["USERNAME"]; ?>"></td><td style="color: #444; background-color: transparent; border: 0; font-weight: bold;">Password: <span style="color: red;">*</span></td><td style="color: #444; background-color: transparent; border: 0; font-weight: normal;"><input id="password" type="password" value=""></td></tr>
+                        <tr><td style="color: #444; background-color: transparent; border: 0; font-weight: bold;">Full Name:</td><td style="color: #444; background-color: transparent; border: 0; font-weight: normal;"> <input id="fullname" type="text" value="<?php print $userinfo["FULLNAME"]; ?>"></td><td style="color: #444; background-color: transparent; border: 0; font-weight: bold;">New Password:</td><td style="color: #444; background-color: transparent; border: 0; font-weight: normal;"><input id="newpassword" type="password"></td></tr>
+                        <tr><td style="color: #444; background-color: transparent; border: 0; font-weight: bold;">Email Address:</td><td style="color: #444; background-color: transparent; border: 0; font-weight: normal;"><input id="email" type="text" value="<?php print $userinfo["EMAIL"]; ?>"></td><td style="color: #444; background-color: transparent; border: 0; font-weight: bold;">Confirm New Password:</td><td style="color: #444; background-color: transparent; border: 0; font-weight: normal;"><input id="confirmnewpass" type="password"></td></tr>
+                        <tr><td style="color: #444; background-color: transparent; border: 0; font-weight: bold;">Phone:</td><td style="color: #444; background-color: transparent; border: 0; font-weight: normal;"><input id="phone" type="text" value="<?php print $userinfo["PHONE"]; ?>"></td><td style="color: #444; background-color: transparent; border: 0; font-weight: bold;">Two-Factor Authentication:</td><td style="color: #444; background-color: transparent; border: 0; font-weight: normal;">
+			<select id="2FA" style="height: 28px; width: 200px; margin-left: 3px;"><option value="enabled">Enabled</option><option value="disabled">Disabled</option></select>
+			</td></tr>
+                        <tr><td style="color: #444; background-color: transparent; border: 0; font-weight: bold;">Role:</td><td style="color: #444; background-color: transparent; border: 0; font-weight: normal;"> <?php print $userrole; ?></td></tr>
+                        <tr><td style="color: #444; background-color: transparent; border: 0; font-weight: bold;">Account Type:</td><td style="color: #444; background-color: transparent; border: 0; font-weight: normal;"><?php print $userinfo["TYPE"]; ?></td></tr>
+                        <tr><td style="color: #444; background-color: transparent; border: 0; font-weight: bold;">Create Date:</td><td style="color: #444; background-color: transparent; border: 0; font-weight: normal;"><?php print $userinfo["REGDATE"]; ?></td></tr>
+
+			</table>
 		</div>
 	</div>
 
