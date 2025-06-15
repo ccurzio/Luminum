183bb849a890de35af8438e72f4e4c1840170fb4
Christopher R. Curzio <ccurzio@accipiter.org>
2025-06-15 18:25:56 -0400

    Added system language detection and initial support for German, Italian, Spanish, and French

--- server/setup.pl
+++ server/setup.pl
@@ -7,12 +7,70 @@ use strict;
 use Curses::UI;
 
 my $text = 0;
-my $cancel = 0;
+my $step = 0;
+my $wizlabel;
+my $wizbuttons;
+my $wizaction;
 my $path = "/opt/Luminum/LuminumServer";
 my $LADDR;
 my $LPORT;
 my $WADDR;
 my $WPORT;
+
+my $introlabel;
+my $nextbutton;
+my $backbutton;
+my $exitbutton;
+my $nextbsc;
+my $backbsc;
+my $exitbsc;
+
+
+if ($ENV{LANG} =~ /en_/ || $ENV{LANG} eq "") {
+	$nextbutton = " Next > ";
+	$backbutton = " < Back ";
+	$exitbutton = " Cancel ";
+	$nextbsc = "N";
+	$backbsc = "B";
+	$exitbsc = "C";
+	$introlabel = "Welcome to the Luminum Server setup wizard. This tool will guide you\nthrough the steps to install Luminum Server on this system.";
+	}
+elsif ($ENV{LANG} =~ /de_/) {
+	$nextbutton = " Nächste > ";
+	$backbutton = "< Zurück ";
+	$exitbutton = " Stornieren ";
+	$nextbsc = "N";
+	$backbsc = "Z";
+	$exitbsc = "S";
+	$introlabel = "Willkommen beim Luminum Server-Setup-Assistenten. Dieses Tool führt\nSie durch die einzelnen Schritte zur Installation von Luminum Server\nauf diesem System.";
+	}
+elsif ($ENV{LANG} =~ /it_/) {
+	$nextbutton = " Prossimo > ";
+	$backbutton = "< Precedente ";
+	$exitbutton = " Cancellare ";
+	$nextbsc = "P";
+	$backbsc = "r";
+	$exitbsc = "C";
+	$introlabel = "Benvenuti alla procedura guidata di installazione di Luminum Server.\nQuesto strumento vi guiderà attraverso i passaggi necessari per\ninstallare Luminum Server su questo sistema.";
+	}
+elsif ($ENV{LANG} =~ /fr_/) {
+	$nextbutton = " Suivant > ";
+	$backbutton = "< Précédent ";
+	$exitbutton = " Annuler ";
+	$nextbsc = "S";
+	$backbsc = "P";
+	$exitbsc = "A";
+	$introlabel = "Bienvenue dans l'assistant d'installation de Luminum Server. Cet\noutil vous guidera pas à pas pour installer Luminum Server\nsur votre système.";
+	}
+elsif ($ENV{LANG} =~ /es_/) {
+	$nextbutton = " Próximo >";
+	$backbutton = "< Previo ";
+	$exitbutton = " Cancelar ";
+	$nextbsc = "P";
+	$backbsc = "v";
+	$exitbsc = "C";
+	$introlabel = "Bienvenido al asistente de configuración de Luminum Server. Esta\nherramienta le guiará por los pasos para instalar Luminum\nServer en este sistema.";
+	}
 
 my $cui = new Curses::UI(-color_support => 1, -intellidraw => 1);
 $cui->set_binding(sub { exit(0); }, "\cC");
@@ -40,16 +98,27 @@ if ($text == 0) {
 		-fg		=> 'black',
 		);
 
-	my $wizbuttons = $wizard->add('wizbuttons','Buttonbox',
+	$wizlabel = $wizard->add('wizlabel', 'Label',
+		-width		=> -1,
+		-height		=> -1,
+		-fg		=> 'black',
+		-padleft	=> 2,
+		-padright	=> 2,
+		-padtop		=> 1,
+		-text		=> $introlabel
+		);
+
+	$wizbuttons = $wizard->add('wizbuttons','Buttonbox',
 		-buttons => [ {
-			-label		=> " Cancel ",
+			-label		=> $exitbutton,
 			-value		=> "exit",
-			-shortcut	=> "C",
+			-shortcut	=> $exitbsc,
 			},
 		{
-			-label		=> " Next ",
+			-label		=> $nextbutton,
 			-value		=> "next",
-			-shortcut	=> "N",
+			-shortcut	=> $nextbsc,
+			-onpress	=> sub { $wizard->delete('wizbuttons'); undef($wizbuttons); }
 			} ],
 		-fg		=> "black",
 		-buttonalignment=> "right",
@@ -59,8 +128,8 @@ if ($text == 0) {
 		-selected	=> 1,
 		);
 
+	my $wizaction = $wizbuttons->get();
 	$wizbuttons->focus();
-	my $wizcancel = $wizbuttons->get();
 
 	$cui->mainloop();
 	}
