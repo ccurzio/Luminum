b96d7c7ee2a60df3b304e24c47c23e7bf1c06c67
Christopher R. Curzio <ccurzio@accipiter.org>
2025-06-16 03:20:52 -0400

    Removed item count placeholder and replaced with dynamic count

--- server/console/html/admin/cgroups.php
+++ server/console/html/admin/cgroups.php
@@ -24,7 +24,7 @@ $cgcount = mysqli_num_rows($cgquery);
 		<button class="formgo" style="margin-top: 5px; margin-right: 0;" disabled="disabled">Get Info</button>
 
 		<table style="margin-top: 10px; text-align: left;">
-		<tr><td colspan="9"><div style="position: absolute; padding-top: 5px; padding-left: 5px;"> 2 of 2 items</div><div style="float: right; text-align: right; padding-right: 5px;">Filter: <input type="text" style="font-size: 15px; padding: 3px; margin-top: 0;" maxlength="64" <?php if ($cgcount == 0) { print "disabled=\"disabled\""; } ?>></div></td></tr>
+		<tr><td colspan="9"><div style="position: absolute; padding-top: 5px; padding-left: 5px;">0 of <?php print $cgcount; ?> items</div><div style="float: right; text-align: right; padding-right: 5px;">Filter: <input type="text" style="font-size: 15px; padding: 3px; margin-top: 0;" maxlength="64" <?php if ($cgcount == 0) { print "disabled=\"disabled\""; } ?>></div></td></tr>
 		<tr><td style="width: 15px; text-align: center; padding: 0;"><?php
 			if ($cgcount == 0) { print "<input type=\"checkbox\" disabled=\"disabled\">"; }
 			else { print "<input id=\"selectall\" type=\"checkbox\" onclick=\"allToggle();\">"; }
