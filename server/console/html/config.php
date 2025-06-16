9c5ace744da1eb3bb52fe2798698dbd5727260d9
Christopher R. Curzio <ccurzio@accipiter.org>
2025-06-16 01:09:04 -0400

    Added config query for listener port; added variable for web port; implemented DNS configuration fetch; added reset button to form; added UI elements for networking configuration

--- server/console/html/config.php
+++ server/console/html/config.php
@@ -9,6 +9,10 @@ else {
 	$hostquery = mysqli_query($db, "select CVAL from CONFIG where CKEY = 'SHOST'");
 	$hostinfo = $hostquery->fetch_assoc();
 	$hostname = $hostinfo["CVAL"];
+	$lpquery = mysqli_query($db, "select CVAL from CONFIG where CKEY = 'LPORT'");
+	$lpinfo = $lpquery->fetch_assoc();
+	$wcport = $_SERVER['SERVER_PORT'];
+	$lport = $lpinfo["CVAL"];
 
 	if (!isset($_GET['view']) || $_GET['view'] == "options"): ?>
 	<?php include ("system/serveropts.php"); ?>
@@ -23,10 +27,16 @@ else {
 		$gateway = rtrim(shell_exec("/usr/sbin/route -n | /usr/bin/grep 'UG[ \t]' | /usr/bin/awk '{print \$2}'"));
 		$gwhostname = preg_replace("/.*pointer /","",shell_exec("/usr/bin/host $gateway"));
 		$gwhostname = rtrim(preg_replace("/\.$/","",$gwhostname));
+		$dnscfg = explode(" ",shell_exec("/usr/bin/cat /etc/resolv.conf | /usr/bin/sed -E 's/(search|nameserver) //g' | /usr/bin/tr '\n' ' '"));
 	?>
 
 	<div class="content">
-	<h1>Networking</h1>
+	<h1>Network Configuration</h1>
+
+	<div style="width: 65%; text-align: right; margin: -50px auto 10px auto;">
+		<button id="save" class="formgo" style="margin-right: 0;" disabled="disabled">Save Changes</button>
+		<input type="reset" id="reset" value="Reset Values" class="formgo" onclick="return resetForm();">
+	</div>
 
 	<div class="module-content" style="padding: 0; width: 60%; margin-left: auto; margin-right: auto; padding: 10px;">
 		<div style="width: 75%; margin-left: auto; margin-right: auto; text-align: center;">
@@ -47,6 +57,17 @@ else {
 			?>
 			</table>
 		</div>
+		<br>
+		<hr style="width: 90%;">
+		<br>
+
+		<table style="border: 0; width: 80%; margin-left: auto; margin-right: auto;">
+		<?php print "<tr><td style=\"color: #444; background-color: transparent; border: 0; font-weight: bold;\">Primary IP Address:</td><td style=\"color: #444; background-color: transparent; border: 0;\"><input id=\"ipaddr\" type=\"text\" style=\"width: 200px; font-size: 15px; padding: 3px; margin-top: 0;\" maxlength=\"39\" value=\"$netifaces[0]\" onchange=\"formCheck();\" tabindex=\"1\"></td><td style=\"color: #444; background-color: transparent; border: 0; font-weight: bold;\">Web Console Port:</td><td style=\"color: #444; background-color: transparent; border: 0;\"><input id=\"wcport\" type=\"text\" style=\"width: 100px; font-size: 15px; padding: 3px; margin-top: 0;\" maxlength=\"5\" value=\"$wcport\" onchange=\"formCheck();\" tabindex=\"6\"></td></tr>\n"; ?>
+		<?php print "<tr><td style=\"color: #444; background-color: transparent; border: 0; font-weight: bold;\">Subnet Mask:</td><td style=\"color: #444; background-color: transparent; border: 0;\"><input id=\"subnetmask\" type=\"text\" style=\"width: 200px; font-size: 15px; padding: 3px; margin-top: 0;\" maxlength=\"39\" value=\"$netifaces[1]\" onchange=\"formCheck();\" tabindex=\"2\"></td><td style=\"color: #444; background-color: transparent; border: 0; font-weight: bold;\">Luminum Port:</td><td style=\"color: #444; background-color: transparent; border: 0;\"><input id=\"lport\" type=\"text\" style=\"width: 100px; font-size: 15px; padding: 3px; margin-top: 0;\" value=\"$lport\" onchange=\"formCheck();\" tabindex=\"7\"></td></tr>\n"; ?>
+		<?php print "<tr><td style=\"color: #444; background-color: transparent; border: 0; font-weight: bold;\">Default Gateway:</td><td style=\"color: #444; background-color: transparent; border: 0;\"><input id=\"dgateway\" type=\"text\" style=\"width: 200px; font-size: 15px; padding: 3px; margin-top: 0;\" maxlength=\"39\" value=\"$gateway\" onchange=\"formCheck();\" tabindex=\"3\"></td></tr>\n"; ?>
+		<?php print "<tr><td style=\"color: #444; background-color: transparent; border: 0; font-weight: bold;\">Primary DNS Server:</td><td style=\"color: #444; background-color: transparent; border: 0;\"><input id=\"dnsserver\" type=\"text\" style=\"width: 200px; font-size: 15px; padding: 3px; margin-top: 0;\" maxlength=\"39\" value=\"$dnscfg[1]\" onchange=\"formCheck();\" tabindex=\"4\"></td></tr>\n"; ?>
+		<?php print "<tr><td style=\"color: #444; background-color: transparent; border: 0; font-weight: bold;\">DNS Search Suffix:</td><td style=\"color: #444; background-color: transparent; border: 0;\"><input id=\"suffix\" type=\"text\" style=\"width: 200px; font-size: 15px; padding: 3px; margin-top: 0;\" maxlength=\"39\" value=\"$dnscfg[0]\" onchange=\"formCheck();\" tabindex=\"5\"></td></tr>\n"; ?>
+		</table>
 	</div>
 
 	<?php elseif ($_GET['view'] == "auth"): ?>
