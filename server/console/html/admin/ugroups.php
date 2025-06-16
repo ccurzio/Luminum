9df996e5ebd0c83b80d6f32450971d0a4915040a
Christopher R. Curzio <ccurzio@accipiter.org>
2025-06-16 03:13:14 -0400

    Created User Groups include page

---
+++ server/console/html/admin/ugroups.php
@@ -0,0 +1,45 @@
+<?php
+mysqli_select_db($db, "AUTH") or die( "<h5>Fatal Error</h5>\n\n<p>Unable to access database.\n</p>");
+
+$starttime = microtime(true);
+$ugquery = mysqli_query($db, "select ID,NAME,AUTHOR,CREATED,MODIFIED,EDITOR from GROUPS order by NAME");
+$endtime = microtime(true);
+
+$duration = number_format((float)$endtime - $starttime, 2, '.', '');
+$ugcount = mysqli_num_rows($ugquery);
+?>
+
+<div class="content">
+
+<?php if ($_GET["view"] == "ugroups"): ?>
+        <?php if (!isset($_GET["action"])): ?>
+
+        <h1>User Groups</h1>
+
+        <div class="module-content">
+		<div style="display: block; width: 100%; text-align: right;">
+		<a href="/index.php?view=ugroups&action=new"><button class="formgo" style="margin-top: 5px; margin-right: 0;">Create New</button></a>
+		<button class="formgo" style="margin-top: 5px; margin-right: 0;" disabled="disabled">Modify Selected</button>
+		<button class="formgo" style="margin-top: 5px; margin-right: 0;" disabled="disabled">Delete Selected</button>
+		<button class="formgo" style="margin-top: 5px; margin-right: 0;" disabled="disabled">Get Info</button>
+
+		<table style="margin-top: 10px; text-align: left;">
+		<tr><td colspan="6"><div style="padding: 6px 0 6px 5px;">0 of 0 items</div></td></tr>
+		<tr>
+		<td style="width: 15px; text-align: center; padding: 0;"><?php
+			if ($ugcount == 0) { print "<input type=\"checkbox\" disabled=\"disabled\">"; }
+			else { print "<input id=\"selectall\" type=\"checkbox\" onclick=\"allToggle();\">"; }
+		?></td>
+		<td style="width: 400px;">Name</td><td style="width: 50px;">Author</td><td style="width: 175px;">Create Date</td><td style="width: 175px;">Last Modification</td><td style="width: 50px;">Modified By</td></tr>
+		<?php
+			if ($ugcount == 0) {
+				print "<tr><td colspan=\"8\" style=\"text-align: center; background-color: #494a69; font-weight: normal; font-style: italic;\">No Results</td></tr>\n";
+				}
+		?>
+		<tr style="height: 35px;"><td colspan="8"><div style="position: absolute; padding-top: 5px;"></div><div style="float: right; text-align: right; padding-bottom: 2px; padding-right: 5px; font-weight: normal;"><i>Query Completed in <?php print $duration; ?> Seconds</i></div></td></tr>
+		</table>
+	</div>
+
+	<?php endif; ?>
+
+<?php endif; ?>
